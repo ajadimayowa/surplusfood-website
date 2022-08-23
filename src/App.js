b91682fe0d75118cc60bd24c152fce5efbe97f83
me <demo>
2022-08-22 21:34:04 -0700

    entry

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  const [hamburger, toggleHamburger] = useState(false)
   return (
 
     <div class="container">
