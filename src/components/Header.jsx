26ba8ba9f3bfb035cf2fe08adec8f0cc1375ba64
me <demo>
2022-08-22 21:26:53 -0700

    Surplus food initialized

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../assets/css/Header.css'
+
+function Header() {
+    return (
+        <div class="header">
+            <p>Header component</p>
+        </div>
+    )
+}
+
+export default Header;
