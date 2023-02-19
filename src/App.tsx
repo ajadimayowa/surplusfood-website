fcfeecb62b7b62d004f63549d31ee38f7d146828
ajadimayowa <samtalktech@outlook.com>
2023-02-19 11:02:37 -0800

    initial  commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid className='bg-primary "min-vh-100"'>
+        <Row className='justify-content-center'>
+          <h1>Welcome To Surplus Food</h1>
+        </Row>
+      </Container>
     </div>
   );
 }
