8c92b4331447f437532f67fbb9f8ea2b7424dba0
ajadimayowa <samtalktech@outlook.com>
2023-02-19 11:05:09 -0800

    initial  commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Container fluid className='bg-primary "min-vh-100"'>
+      <Container fluid className='bg-primary min-vh-100'>
         <Row className='justify-content-center'>
           <h1>Welcome To Surplus Food</h1>
         </Row>
